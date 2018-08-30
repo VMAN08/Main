4a891f6107dc7e1769f05119da3140aa5469053b
vikra <vikra@172.31.20.194>
2018-08-30 14:58:25 -0700

    Assignment 1 - Height from feet to meters

---
+++ Homework/src/hwAssignments/Assignment1.java
@@ -0,0 +1,27 @@
+package hwAssignments;
+import java.util.Scanner;
+
+/*Answers to the Written Exercises
+ * 
+ * 
+ */
+
+
+public class Assignment1 
+{
+	
+	public static void main(String[] args)
+	{
+	
+		Scanner user_input = new Scanner(System.in);
+		String name;
+		int height_feet;
+		int height_inches;
+		double height_meters;
+		
+		System.out.println("What is your name: ");
+		name = 
+		
+	}
+
+}
