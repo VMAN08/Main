6e776b0418cce98f0686cc123d1d430db403a8f0
vikra <vikra@172.31.20.194>
2018-08-30 16:25:43 -0700

    (Edit) Finished program

--- Homework/src/hwAssignments/Assignment1.java
+++ Homework/src/hwAssignments/Assignment1.java
@@ -1,9 +1,28 @@
 package hwAssignments;
 import java.util.Scanner;
 
+/*
+// AUTHOR: Vikram Dattatri
+// FILENAME: Assignment1.java
+// SPECIFICATION: This program takes the user's name and height and converts it from feet to meters
+// FOR: CSE-110 Assignment 1
+// TIME SPENT: 20 minutes
+*/
+
 /*Answers to the Written Exercises
+ * 1. The value of mystery will be 2
  * 
+ * 2. a. 10
+ * 	  b. 1
+ *    c. 27
+ *    d. 27.5
+ *    e. 27
+ *    f. 28
  * 
+ * 3.
+ *  Scanner user_input = new Scanner(System.in);
+ *  System.out.println("Please provide your full name: ");
+ *  String name = user_input.nextLine();
  */
 
 
@@ -19,8 +38,19 @@ public class Assignment1
 		int height_inches;
 		double height_meters;
 		
+		
 		System.out.println("What is your name: ");
-		name = 
+		name = user_input.nextLine();
+		System.out.println("What is your height?");
+		System.out.println("Feet: ");
+		height_feet = user_input.nextInt();
+		System.out.println("Inches: ");
+		height_inches = user_input.nextInt();
+		
+		height_meters = ((height_feet*12) + height_inches) * 2.54/100;
+		
+		System.out.println(name + ", your height in meters is " + height_meters);
+		
 		
 	}
 
