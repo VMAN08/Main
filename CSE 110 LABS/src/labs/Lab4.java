ab032a0108401952ee4422b956c5bf082a19a8cc
vikra <vikra@vikramslaptop>
2018-09-25 16:16:21 -0700

    Lab 4

---
+++ CSE 110 LABS/src/labs/Lab4.java
@@ -0,0 +1,56 @@
+package labs;
+import java.util.*;
+
+/*
+//AUTHOR: Vikram Dattatri
+//FILENAME: Lab4.java
+//SPECIFICATION: 
+//FOR: CSE-110 Lab 4
+//TIME SPENT:  minutes
+*/
+
+
+public class Lab4 {
+	
+	public static void main(String[] args)
+	
+	{
+		
+		int choice;
+		Scanner user_input = new Scanner(System.in);
+		
+		do {
+			
+			System.out.println("Please choose your choice from the following menu: ");
+			System.out.println("1) Calculate the sum of integers 1 to m.");
+			System.out.println("3) Calculate the factorial of given number.");
+			System.out.println("3) Find the first digit of a given number.");
+			System.out.println("4) Quit.");
+			choice = user_input.nextInt();
+			
+		} while (choice != 4);
+		
+		
+		
+		switch (choice)
+		{
+		
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+			
+			
+
+
+			
+		}
+			
+		
+	}
+
+}
